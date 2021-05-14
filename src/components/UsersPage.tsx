bdbfbb479b2e2e68b0d47ce02822f776866d1b02
nsandor-webDev <96.sandor.norbert@gmail.com>
2021-05-14 13:02:43 +0200

    subpage, users detail, only CSS missing

--- src/components/UsersPage.tsx
+++ src/components/UsersPage.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useLocation, RouteComponentProps  } from 'react-router-dom';
 
-interface Props {
-    
+type StateType = {
+    id: number,
+    imgSrc: string,
+    name: string,
+    address: string
 }
 
-const UsersPage = (props: Props) => {
+type IndexProps = RouteComponentProps<{}, {}, StateType>;
+interface Props {
+
+}
+
+const UsersPage: React.FC<IndexProps> = ({ location, history }) => {
     return (
         <div>
-            <h1>Subpage :)</h1>
+            <img src={location.state.imgSrc} alt="" />
+            <h1>{location.state.name}</h1>
+            <p>{location.state.address}</p>
+            <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quos laudantium natus amet hic perferendis enim inventore incidunt blanditiis, optio magnam aliquid laborum impedit officiis vero a in quaerat recusandae?</p>
+
         </div>
     )
 }
